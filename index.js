96d6ac1b247136b1e65de84cdc5b5f4fbd4d5e42
abcxomisgood <53421128+abcxomisgood@users.noreply.github.com>
2020-03-26 20:15:22 -0700

    Keep running into errors

--- index.js
+++
@@ -1,6 +0,0 @@
-const Discord = requre("discord.js");
-const bot = new Discord.Client();
-
-const token = 'NjkyOTE5NzUxMDM4NTk5MjQ5.Xn1h2A.2ObTeSl7p4o6TUrCrLUvEiB94ys';
-
-bot.login(process.env.token);
